42251e1902a9719b51d22c308620a93dd221a8ce
Martin Blažek <martinblazek6@gmail.com>
2021-11-29 11:15:09 +0100

    Conditions upload

--- Advicer/src/Advicer.java
+++ Advicer/src/Advicer.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Advicer {
     public static void main(String[] args){
+
+        //List of clubs
+            //Martin - Football
+            String[] freeClubsFootball = {"FC Nam sa nechce", "Footballers"};
+            String[] paidClubsFootball = {"FCMartin", "FCParadise"};
+
+            //Martin - Hockey
+            String[] freeClubsHockey = {"HC Martin"};
+            String[] paidClubsHockey = {"MHC Martin", "Ice Breakers"};
+
+            //Martin - martial arts
+            String[] freeClubsMA = {"Mortal Combat", "Tekken", "Hospoda u Honzu"};
+            String[] paidClubsMA = {"Octagon Martin", "Spartans", "Avengers"};
+        //List of cities
+        String[] cities = {"Martin", "Bratislava", "Brno", "Prague"};
+        //list of sports
         String advice = "Hmm i think you should try : ";
         String option1 = "ice hockey";
         String option2 = "martial arts";
@@ -12,6 +30,7 @@ public class Advicer {
         String option5 = "Curling";
         boolean Statecheck = true;
         boolean ClubCash = true;
+        String ChoosenSport = null;
         System.out.print("Do you like to run a lot or you prefer less movement in sport? 1-movement 2-less movement : ");
         Scanner my_scan = new Scanner(System.in);
         char movement =  my_scan.next().charAt(0);
@@ -27,34 +46,49 @@ public class Advicer {
 
         if (result.equals("1ywm")) {
             System.out.print(advice + option1);
+            ChoosenSport = option1;
         }
         else if (result.equals("2nsl")) {
             System.out.print(advice + option2);
+            ChoosenSport = option2;
         }
         else if (result.equals("1ysl")) {
             System.out.print(advice + option3);
+            ChoosenSport = option3;
         }
         else if (result.equals("1nwl")) {
             System.out.print(advice + option4);
+            ChoosenSport = option4;
         }
         else if (result.equals("2ywl")) {
             System.out.print(advice + option5);
+            ChoosenSport = option5;
         } else {System.out.println("I can not help you right now :(");
             Statecheck = false;}
 
-        if (Statecheck == true){
-        Scanner my_scan_String = new Scanner(System.in);
-        System.out.print("\nIn which city do you want to train? : ");
-        String CityClub =  my_scan_String.next().toString();
-            System.out.print("Can you afford to pay monthly or do you want to train fo free? Leave blank input if you don't mind : ");
-            String Mone2Club =  my_scan_String.next().toString();
-            if (Mone2Club == "free") {ClubCash = false;}
-        if (CityClub == "Martin")
-        System.out.print(CityClub);
-            if (ClubCash){
-                System.out.print("Paid club");
+        if (Statecheck == true) {
+            Scanner my_scan_String = new Scanner(System.in);
+            System.out.print("\nIn which city do you want to train? : ");
+            String CityClub = my_scan_String.next();
+            System.out.print("Can you afford to pay monthly or do you want to train fo free? Leave * as input if you don't mind options:paid/fre : ");
+            String Mone2Club = my_scan_String.next();
+            //System.out.print(Mone2Club);
+            if (Objects.equals(CityClub, "Martin")) {
+                if (ChoosenSport.equals(option3)){
+                    if (Objects.equals(Mone2Club, "paid")) {
+                        System.out.print("Here are some nonfree clubs for : " + ChoosenSport + "\n");
+                        System.out.println(Arrays.toString(paidClubsFootball));
+                    } else if (Objects.equals(Mone2Club, "*")) {
+                        System.out.print("Here are some free clubs for: " + ChoosenSport + "\n");
+                        System.out.println(Arrays.toString(freeClubsFootball));
+                        System.out.print("Here are some nonfree clubs for:  " + ChoosenSport + "\n");
+                        System.out.println(Arrays.toString(paidClubsFootball));
+                    } else if (Objects.equals(Mone2Club, "free")) {
+                        System.out.print("Here are some free clubs for:  " + ChoosenSport + "\n");
+                        System.out.println(Arrays.toString(freeClubsFootball));
+                    }
+                }
             }
-            else {System.out.print("NonPaid club");}
         }
 
 
